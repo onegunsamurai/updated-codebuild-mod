441fd320840df6f94147fa559995bc3de6f7a125
onegunsamurai <89980150+onegunsamurai@users.noreply.github.com>
2021-11-08 13:45:04 -0500

    Init

---
+++ main.tf
@@ -0,0 +1,486 @@
+data "aws_caller_identity" "default" {
+}
+
+data "aws_region" "default" {
+}
+
+resource "aws_s3_bucket" "cache_bucket" {
+  #bridgecrew:skip=BC_AWS_S3_13:Skipping `Enable S3 Bucket Logging` check until bridgecrew will support dynamic blocks (https://github.com/bridgecrewio/checkov/issues/776).
+  #bridgecrew:skip=BC_AWS_S3_14:Skipping `Ensure all data stored in the S3 bucket is securely encrypted at rest` check until bridgecrew will support dynamic blocks (https://github.com/bridgecrewio/checkov/issues/776).
+  #bridgecrew:skip=CKV_AWS_52:Skipping `Ensure S3 bucket has MFA delete enabled` due to issue in terraform (https://github.com/hashicorp/terraform-provider-aws/issues/629).
+  count         = module.this.enabled && local.s3_cache_enabled ? 1 : 0
+  bucket        = local.cache_bucket_name_normalised
+  acl           = "private"
+  force_destroy = true
+  tags          = module.this.tags
+
+  versioning {
+    enabled = var.versioning_enabled
+  }
+
+  dynamic "logging" {
+    for_each = var.access_log_bucket_name != "" ? [1] : []
+    content {
+      target_bucket = var.access_log_bucket_name
+      target_prefix = "logs/${module.this.id}/"
+    }
+  }
+
+  lifecycle_rule {
+    id      = "codebuildcache"
+    enabled = true
+
+    prefix = "/"
+    tags   = module.this.tags
+
+    expiration {
+      days = var.cache_expiration_days
+    }
+  }
+
+  dynamic "server_side_encryption_configuration" {
+    for_each = var.encryption_enabled ? ["true"] : []
+
+    content {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+  }
+}
+
+resource "random_string" "bucket_prefix" {
+  count   = module.this.enabled ? 1 : 0
+  length  = 12
+  number  = false
+  upper   = false
+  special = false
+  lower   = true
+}
+
+locals {
+  cache_bucket_name = "${module.this.id}${var.cache_bucket_suffix_enabled ? "-${join("", random_string.bucket_prefix.*.result)}" : ""}"
+
+  ## Clean up the bucket name to use only hyphens, and trim its length to 63 characters.
+  ## As per https://docs.aws.amazon.com/AmazonS3/latest/dev/BucketRestrictions.html
+  cache_bucket_name_normalised = substr(
+    join("-", split("_", lower(local.cache_bucket_name))),
+    0,
+    min(length(local.cache_bucket_name), 63),
+  )
+
+  s3_cache_enabled = var.cache_type == "S3"
+
+  ## This is the magic where a map of a list of maps is generated
+  ## and used to conditionally add the cache bucket option to the
+  ## aws_codebuild_project
+  cache_options = {
+    "S3" = {
+      type     = "S3"
+      location = module.this.enabled && local.s3_cache_enabled ? join("", aws_s3_bucket.cache_bucket.*.bucket) : "none"
+
+    },
+    "LOCAL" = {
+      type  = "LOCAL"
+      modes = var.local_cache_modes
+    },
+    "NO_CACHE" = {
+      type = "NO_CACHE"
+    }
+  }
+
+  # Final Map Selected from above
+  cache = local.cache_options[var.cache_type]
+}
+
+resource "aws_iam_role" "default" {
+  count                 = module.this.enabled ? 1 : 0
+  name                  = module.this.id
+  assume_role_policy    = data.aws_iam_policy_document.role.json
+  force_detach_policies = true
+  tags                  = module.this.tags
+}
+
+data "aws_iam_policy_document" "role" {
+  statement {
+    sid = ""
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_policy" "default" {
+  count  = module.this.enabled ? 1 : 0
+  name   = module.this.id
+  path   = "/service-role/"
+  policy = data.aws_iam_policy_document.combined_permissions.json
+}
+
+resource "aws_iam_policy" "default_cache_bucket" {
+  count = module.this.enabled && local.s3_cache_enabled ? 1 : 0
+
+
+  name   = "${module.this.id}-cache-bucket"
+  path   = "/service-role/"
+  policy = join("", data.aws_iam_policy_document.permissions_cache_bucket.*.json)
+}
+
+data "aws_s3_bucket" "secondary_artifact" {
+  count  = module.this.enabled ? (var.secondary_artifact_location != null ? 1 : 0) : 0
+  bucket = var.secondary_artifact_location
+}
+
+data "aws_iam_policy_document" "permissions" {
+  count = module.this.enabled ? 1 : 0
+
+  statement {
+    sid = ""
+
+    actions = compact(concat([
+      "codecommit:GitPull",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetAuthorizationToken",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:UploadLayerPart",
+      "ecs:RunTask",
+      "iam:PassRole",
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "ssm:GetParameters",
+      "secretsmanager:GetSecretValue",
+    ], var.extra_permissions))
+
+    effect = "Allow"
+
+    resources = [
+      "*",
+    ]
+  }
+
+  dynamic "statement" {
+    for_each = var.secondary_artifact_location != null ? [1] : []
+    content {
+      sid = ""
+
+      actions = [
+        "s3:PutObject",
+        "s3:GetBucketAcl",
+        "s3:GetBucketLocation"
+      ]
+
+      effect = "Allow"
+
+      resources = [
+        join("", data.aws_s3_bucket.secondary_artifact.*.arn),
+        "${join("", data.aws_s3_bucket.secondary_artifact.*.arn)}/*",
+      ]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "vpc_permissions" {
+  count = module.this.enabled && var.vpc_config != {} ? 1 : 0
+
+  statement {
+    sid = ""
+
+    actions = [
+      "ec2:CreateNetworkInterface",
+      "ec2:DescribeDhcpOptions",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DeleteNetworkInterface",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeVpcs"
+    ]
+
+    resources = [
+      "*",
+    ]
+  }
+
+  statement {
+    sid = ""
+
+    actions = [
+      "ec2:CreateNetworkInterfacePermission"
+    ]
+
+    resources = [
+      "arn:aws:ec2:${var.aws_region}:${var.aws_account_id}:network-interface/*"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "ec2:Subnet"
+      values = formatlist(
+        "arn:aws:ec2:${var.aws_region}:${var.aws_account_id}:subnet/%s",
+        var.vpc_config.subnets
+      )
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "ec2:AuthorizedService"
+      values = [
+        "codebuild.amazonaws.com"
+      ]
+    }
+
+  }
+}
+
+data "aws_iam_policy_document" "combined_permissions" {
+  override_policy_documents = compact([
+    join("", data.aws_iam_policy_document.permissions.*.json),
+    var.vpc_config != {} ? join("", data.aws_iam_policy_document.vpc_permissions.*.json) : null
+  ])
+}
+
+data "aws_iam_policy_document" "permissions_cache_bucket" {
+  count = module.this.enabled && local.s3_cache_enabled ? 1 : 0
+  statement {
+    sid = ""
+
+    actions = [
+      "s3:*",
+    ]
+
+    effect = "Allow"
+
+    resources = [
+      join("", aws_s3_bucket.cache_bucket.*.arn),
+      "${join("", aws_s3_bucket.cache_bucket.*.arn)}/*",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "default" {
+  count      = module.this.enabled ? 1 : 0
+  policy_arn = join("", aws_iam_policy.default.*.arn)
+  role       = join("", aws_iam_role.default.*.id)
+}
+
+resource "aws_iam_role_policy_attachment" "default_cache_bucket" {
+  count      = module.this.enabled && local.s3_cache_enabled ? 1 : 0
+  policy_arn = join("", aws_iam_policy.default_cache_bucket.*.arn)
+  role       = join("", aws_iam_role.default.*.id)
+}
+
+resource "aws_codebuild_source_credential" "authorization" {
+  count       = module.this.enabled && var.private_repository ? 1 : 0
+  auth_type   = var.source_credential_auth_type
+  server_type = var.source_credential_server_type
+  token       = var.source_credential_token
+  user_name   = var.source_credential_user_name
+}
+
+resource "aws_codebuild_webhook" "example" {
+  project_name = aws_codebuild_project.deafult.name
+  build_type   = "BUILD"
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PUSH"
+    }
+
+    filter {
+      type    = "HEAD_REF"
+      pattern = "main"
+    }
+  }
+}
+
+resource "aws_codebuild_project" "default" {
+  count          = module.this.enabled ? 1 : 0
+  name           = module.this.id
+  service_role   = join("", aws_iam_role.default.*.arn)
+  badge_enabled  = var.badge_enabled
+  build_timeout  = var.build_timeout
+  source_version = var.source_version != "" ? var.source_version : null
+  tags = {
+    for name, value in module.this.tags :
+    name => value
+    if length(value) > 0
+  }
+
+  artifacts {
+    type     = var.artifact_type
+    location = var.artifact_location
+  }
+
+  # Since the output type is restricted to S3 by the provider (this appears to
+  # be an bug in AWS, rather than an architectural decision; see this issue for
+  # discussion: https://github.com/hashicorp/terraform-provider-aws/pull/9652),
+  # this cannot be a CodePipeline output. Otherwise, _all_ of the artifacts
+  # would need to be secondary if there were more than one. For reference, see
+  # https://docs.aws.amazon.com/codepipeline/latest/userguide/action-reference-CodeBuild.html#action-reference-CodeBuild-config.
+  dynamic "secondary_artifacts" {
+    for_each = var.secondary_artifact_location != null ? [1] : []
+    content {
+      type                = "S3"
+      location            = var.secondary_artifact_location
+      artifact_identifier = var.secondary_artifact_identifier
+      encryption_disabled = ! var.secondary_artifact_encryption_enabled
+      # According to AWS documention, in order to have the artifacts written
+      # to the root of the bucket, the 'namespace_type' should be 'NONE'
+      # (which is the default), 'name' should be '/', and 'path' should be
+      # empty. For reference, see https://docs.aws.amazon.com/codebuild/latest/APIReference/API_ProjectArtifacts.html.
+      # However, I was unable to get this to deploy to the root of the bucket
+      # unless path was also set to '/'.
+      path = "/"
+      name = "/"
+    }
+  }
+
+  cache {
+    type     = lookup(local.cache, "type", null)
+    location = lookup(local.cache, "location", null)
+    modes    = lookup(local.cache, "modes", null)
+  }
+
+  environment {
+    compute_type    = var.build_compute_type
+    image           = var.build_image
+    type            = var.build_type
+    privileged_mode = var.privileged_mode
+
+    environment_variable {
+      name  = "AWS_REGION"
+      value = signum(length(var.aws_region)) == 1 ? var.aws_region : data.aws_region.default.name
+    }
+
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = signum(length(var.aws_account_id)) == 1 ? var.aws_account_id : data.aws_caller_identity.default.account_id
+    }
+
+    dynamic "environment_variable" {
+      for_each = signum(length(var.image_repo_name)) == 1 ? [""] : []
+      content {
+        name  = "IMAGE_REPO_NAME"
+        value = var.image_repo_name
+      }
+    }
+
+    dynamic "environment_variable" {
+      for_each = signum(length(var.image_tag)) == 1 ? [""] : []
+      content {
+        name  = "IMAGE_TAG"
+        value = var.image_tag
+      }
+    }
+
+    dynamic "environment_variable" {
+      for_each = signum(length(module.this.stage)) == 1 ? [""] : []
+      content {
+        name  = "STAGE"
+        value = module.this.stage
+      }
+    }
+
+    dynamic "environment_variable" {
+      for_each = signum(length(var.github_token)) == 1 ? [""] : []
+      content {
+        name  = "GITHUB_TOKEN"
+        value = var.github_token
+        type  = var.github_token_type
+      }
+    }
+
+    dynamic "environment_variable" {
+      for_each = var.environment_variables
+      content {
+        name  = environment_variable.value.name
+        value = environment_variable.value.value
+        type  = environment_variable.value.type
+      }
+    }
+
+  }
+
+  source {
+    buildspec           = var.buildspec
+    type                = var.source_type
+    location            = var.source_location
+    report_build_status = var.report_build_status
+    git_clone_depth     = var.git_clone_depth != null ? var.git_clone_depth : null
+
+    dynamic "auth" {
+      for_each = var.private_repository ? [""] : []
+      content {
+        type     = "OAUTH"
+        resource = join("", aws_codebuild_source_credential.authorization.*.id)
+      }
+    }
+
+    dynamic "git_submodules_config" {
+      for_each = var.fetch_git_submodules ? [""] : []
+      content {
+        fetch_submodules = true
+      }
+    }
+  }
+
+  dynamic "secondary_sources" {
+    for_each = var.secondary_sources
+    content {
+      git_clone_depth     = secondary_source.value.git_clone_depth
+      location            = secondary_source.value.location
+      source_identifier   = secondary_source.value.source_identifier
+      type                = secondary_source.value.type
+      insecure_ssl        = secondary_source.value.insecure_ssl
+      report_build_status = secondary_source.value.report_build_status
+
+      git_submodules_config {
+        fetch_submodules = secondary_source.value.fetch_submodules
+      }
+    }
+  }
+
+  dynamic "vpc_config" {
+    for_each = length(var.vpc_config) > 0 ? [""] : []
+    content {
+      vpc_id             = lookup(var.vpc_config, "vpc_id", null)
+      subnets            = lookup(var.vpc_config, "subnets", null)
+      security_group_ids = lookup(var.vpc_config, "security_group_ids", null)
+    }
+  }
+
+  dynamic "logs_config" {
+    for_each = length(var.logs_config) > 0 ? [""] : []
+    content {
+      dynamic "cloudwatch_logs" {
+        for_each = contains(keys(var.logs_config), "cloudwatch_logs") ? { key = var.logs_config["cloudwatch_logs"] } : {}
+        content {
+          status      = lookup(cloudwatch_logs.value, "status", null)
+          group_name  = lookup(cloudwatch_logs.value, "group_name", null)
+          stream_name = lookup(cloudwatch_logs.value, "stream_name", null)
+        }
+      }
+
+      dynamic "s3_logs" {
+        for_each = contains(keys(var.logs_config), "s3_logs") ? { key = var.logs_config["s3_logs"] } : {}
+        content {
+          status              = lookup(s3_logs.value, "status", null)
+          location            = lookup(s3_logs.value, "location", null)
+          encryption_disabled = lookup(s3_logs.value, "encryption_disabled", null)
+        }
+      }
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,265 @@
+variable "environment_variables" {
+  type = list(object(
+    {
+      name  = string
+      value = string
+      type  = string
+  }))
+
+  default = [
+    {
+      name  = "NO_ADDITIONAL_BUILD_VARS"
+      value = "TRUE"
+      type  = "PLAINTEXT"
+  }]
+
+  description = "A list of maps, that contain the keys 'name', 'value', and 'type' to be used as additional environment variables for the build. Valid types are 'PLAINTEXT', 'PARAMETER_STORE', or 'SECRETS_MANAGER'"
+}
+
+variable "cache_expiration_days" {
+  default     = 7
+  description = "How many days should the build cache be kept. It only works when cache_type is 'S3'"
+}
+
+variable "cache_bucket_suffix_enabled" {
+  type        = bool
+  default     = true
+  description = "The cache bucket generates a random 13 character string to generate a unique bucket name. If set to false it uses terraform-null-label's id value. It only works when cache_type is 'S3"
+}
+
+variable "cache_type" {
+  type        = string
+  default     = "NO_CACHE"
+  description = "The type of storage that will be used for the AWS CodeBuild project cache. Valid values: NO_CACHE, LOCAL, and S3.  Defaults to NO_CACHE.  If cache_type is S3, it will create an S3 bucket for storing codebuild cache inside"
+}
+
+variable "local_cache_modes" {
+  type        = list(string)
+  default     = []
+  description = "Specifies settings that AWS CodeBuild uses to store and reuse build dependencies. Valid values: LOCAL_SOURCE_CACHE, LOCAL_DOCKER_LAYER_CACHE, and LOCAL_CUSTOM_CACHE"
+}
+
+variable "badge_enabled" {
+  type        = bool
+  default     = false
+  description = "Generates a publicly-accessible URL for the projects build badge. Available as badge_url attribute when enabled"
+}
+
+variable "build_image" {
+  type        = string
+  default     = "aws/codebuild/standard:2.0"
+  description = "Docker image for build environment, e.g. 'aws/codebuild/standard:2.0' or 'aws/codebuild/eb-nodejs-6.10.0-amazonlinux-64:4.0.0'. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref.html"
+}
+
+variable "build_compute_type" {
+  type        = string
+  default     = "BUILD_GENERAL1_SMALL"
+  description = "Instance type of the build instance"
+}
+
+variable "build_timeout" {
+  default     = 60
+  description = "How long in minutes, from 5 to 480 (8 hours), for AWS CodeBuild to wait until timing out any related build that does not get marked as completed"
+}
+
+variable "build_type" {
+  type        = string
+  default     = "LINUX_CONTAINER"
+  description = "The type of build environment, e.g. 'LINUX_CONTAINER' or 'WINDOWS_CONTAINER'"
+}
+
+variable "buildspec" {
+  type        = string
+  default     = ""
+  description = "Optional buildspec declaration to use for building the project"
+}
+
+variable "privileged_mode" {
+  type        = bool
+  default     = false
+  description = "(Optional) If set to true, enables running the Docker daemon inside a Docker container on the CodeBuild instance. Used when building Docker images"
+}
+
+variable "github_token" {
+  type        = string
+  default     = ""
+  description = "(Optional) GitHub auth token environment variable (`GITHUB_TOKEN`)"
+}
+
+variable "github_token_type" {
+  type        = string
+  default     = "PARAMETER_STORE"
+  description = "Storage type of GITHUB_TOKEN environment variable (`PARAMETER_STORE`, `PLAINTEXT`, `SECRETS_MANAGER`)"
+}
+
+variable "aws_region" {
+  type        = string
+  default     = ""
+  description = "(Optional) AWS Region, e.g. us-east-1. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "aws_account_id" {
+  type        = string
+  default     = ""
+  description = "(Optional) AWS Account ID. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "image_repo_name" {
+  type        = string
+  default     = "UNSET"
+  description = "(Optional) ECR repository name to store the Docker image built by this module. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "image_tag" {
+  type        = string
+  default     = "latest"
+  description = "(Optional) Docker image tag in the ECR repository, e.g. 'latest'. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "secondary_sources" {
+  type = list(object(
+    {
+      git_clone_depth     = number
+      location            = string
+      source_identifier   = string
+      type                = string
+      fetch_submodules    = bool
+      insecure_ssl        = bool
+      report_build_status = bool
+  }))
+  default     = []
+  description = "(Optional) secondary source for the codebuild project in addition to the primary location"
+}
+
+variable "source_type" {
+  type        = string
+  default     = "CODEPIPELINE"
+  description = "The type of repository that contains the source code to be built. Valid values for this parameter are: CODECOMMIT, CODEPIPELINE, GITHUB, GITHUB_ENTERPRISE, BITBUCKET or S3"
+}
+
+variable "source_location" {
+  type        = string
+  default     = ""
+  description = "The location of the source code from git or s3"
+}
+
+variable "artifact_type" {
+  type        = string
+  default     = "CODEPIPELINE"
+  description = "The build output artifact's type. Valid values for this parameter are: CODEPIPELINE, NO_ARTIFACTS or S3"
+}
+
+variable "artifact_location" {
+  type        = string
+  default     = ""
+  description = "Location of artifact. Applies only for artifact of type S3"
+}
+
+variable "secondary_artifact_location" {
+  type        = string
+  default     = null
+  description = "Location of secondary artifact. Must be an S3 reference"
+}
+
+variable "secondary_artifact_identifier" {
+  type        = string
+  default     = null
+  description = "Secondary artifact identifier. Must match the identifier in the build spec"
+}
+
+variable "secondary_artifact_encryption_enabled" {
+  type        = bool
+  default     = false
+  description = "Set to true to enable encryption on the secondary artifact bucket"
+}
+
+variable "report_build_status" {
+  type        = bool
+  default     = false
+  description = "Set to true to report the status of a build's start and finish to your source provider. This option is only valid when the source_type is BITBUCKET or GITHUB"
+}
+
+variable "git_clone_depth" {
+  type        = number
+  default     = null
+  description = "Truncate git history to this many commits."
+}
+
+variable "private_repository" {
+  type        = bool
+  default     = false
+  description = "Set to true to login into private repository with credentials supplied in source_credential variable."
+}
+
+variable "source_credential_auth_type" {
+  type        = string
+  default     = "PERSONAL_ACCESS_TOKEN"
+  description = "The type of authentication used to connect to a GitHub, GitHub Enterprise, or Bitbucket repository."
+}
+
+variable "source_credential_server_type" {
+  type        = string
+  default     = "GITHUB"
+  description = "The source provider used for this project."
+}
+
+variable "source_credential_token" {
+  type        = string
+  default     = ""
+  description = "For GitHub or GitHub Enterprise, this is the personal access token. For Bitbucket, this is the app password."
+}
+
+variable "source_credential_user_name" {
+  type        = string
+  default     = ""
+  description = "The Bitbucket username when the authType is BASIC_AUTH. This parameter is not valid for other types of source providers or connections."
+}
+
+variable "source_version" {
+  type        = string
+  default     = ""
+  description = "A version of the build input to be built for this project. If not specified, the latest version is used."
+}
+
+variable "fetch_git_submodules" {
+  type        = bool
+  default     = false
+  description = "If set to true, fetches Git submodules for the AWS CodeBuild build project."
+}
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/codebuild_project#vpc_config
+variable "vpc_config" {
+  type        = any
+  default     = {}
+  description = "Configuration for the builds to run inside a VPC."
+}
+
+variable "logs_config" {
+  type        = any
+  default     = {}
+  description = "Configuration for the builds to store log data to CloudWatch or S3."
+}
+
+variable "extra_permissions" {
+  type        = list(any)
+  default     = []
+  description = "List of action strings which will be added to IAM service account permissions."
+}
+
+variable "encryption_enabled" {
+  type        = bool
+  default     = false
+  description = "When set to 'true' the resource will have AES256 encryption enabled by default"
+}
+
+variable "versioning_enabled" {
+  type        = bool
+  default     = true
+  description = "A state of versioning. Versioning is a means of keeping multiple variants of an object in the same bucket"
+}
+
+variable "access_log_bucket_name" {
+  type        = string
+  default     = ""
+  description = "Name of the S3 bucket where s3 access log will be sent to"
+}
